878205dc2b9f4098894bcbef4496b573fb33b889
sorrachak.holy201 <sorrachak.holy201@gmail.com>
2024-02-08 03:39:05 +0700

    fist commit -b staging

---
+++ environments/staging/google-staging-k8s-deployment-rollout-status.sh
@@ -0,0 +1,16 @@
+############### k8s-deployment-rollout-status.sh ###############
+#!/bin/bash
+
+#k8s-deployment-rollout-status.sh
+
+sleep 60s
+
+if [[ $(./kubectl -n staging-vizelog rollout status deploy ${deploymentName} --timeout 10s) != *"successfully rolled out"* ]]; 
+then     
+	echo "Deployment ${deploymentName} Rollout has Failed"
+    ./kubectl -n staging-vizelog rollout undo deploy ${deploymentName}
+    exit 1;
+else
+	echo "Deployment ${deploymentName} Rollout is Success"
+fi
+############### k8s-deployment-rollout-status.sh ###############
